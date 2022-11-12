1e78d9e26b94667affcaa0ba176dc42f19e8d21f
Vladislav <dionris@icloud.com>
2022-11-12 16:03:28 +0300

    007 search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -20,7 +20,8 @@ class App extends Component {
                 {name: 'Danik', salary: 1300, increase: true, rise: false, id: 2},
                 {name: 'Dima', salary: 1500, increase: false, rise: false, id: 3},
         
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
 
@@ -135,21 +136,37 @@ class App extends Component {
         }))    
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
+
         return (
             <div className="app">
                 <AppInfo employees={employees} 
                          increased={increased}/>
 
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
                     
                 <EmployeesList 
-                data={this.state.data}
+                data={visibleData}
                 onDelete={this.deleteItem}
 
                 //no optimization
